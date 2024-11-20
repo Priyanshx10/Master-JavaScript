46de5a71ced02a9310575644b09216165ff68431
Priyansh <Priyanshyadav1012@gmail.com>
2024-11-20 12:36:17 +0530

    Variable

--- 00. Intial/index.js
+++ 00. Intial/index.js
@@ -1,3 +1,11 @@
 console.log("Hello");
 
 alert("Hello Coders");
+
+console.warn("This is a Warning");
+
+console.table({ name: "Priyansh", age: 20 });
+
+console.clear();
+
+console.log("Console is an Object with multiple methods like log,clear,warn");
